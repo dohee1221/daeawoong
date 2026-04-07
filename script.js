313346ccd382c01ca9198a9f2aa1b9ef5beb8d38
dohee1221 <dohee1221@gmail.com>
2026-04-08 07:09:09 +0900

    플로팅 끝?, 탑바 움직이는거 필요

--- script.js
+++ script.js
@@ -39,6 +39,31 @@ $('.top-bar-menu__npall .btn').click(function(){
     $('body').toggleClass('is-locked');
 });
 
+    //floating 
+$('.main_home_floating > a').click(function(){
+    let $this = $(this);
+
+    $('.main_home_floating > a').removeClass('on');
+    $this.addClass('on');
+})
+
+// 스크롤 시 플로팅 메뉴 활성화 (Scrollspy)
+$(window).on('scroll', function() {
+    let scrollTop = $(window).scrollTop() + 200; 
+
+    $('.main_sec').each(function() {
+        let $this = $(this);
+        let sectionTop = $this.offset().top; 
+        let sectionBottom = sectionTop + $this.outerHeight(); 
+        let sectionId = $this.attr('id'); 
+
+        if (scrollTop >= sectionTop && scrollTop < sectionBottom) {
+            $('.main_home_floating > a').removeClass('on');
+            $('.main_home_floating > a[href="#' + sectionId + '"]').addClass('on');
+        }
+    });
+});
+
     //main_sec2 hover
 
     var i = 0;
