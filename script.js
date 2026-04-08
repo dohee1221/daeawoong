b61a6ac0a7e95c1b530324679522de5981ef6709
dohee1221 <dohee1221@gmail.com>
2026-04-08 18:16:42 +0900

    완성, 피드백 전

--- script.js
+++ script.js
@@ -58,28 +58,29 @@ $('.top-bar-menu__npall .btn').click(function(){
 });
 
     //floating 
-$('.main_home_floating > a').click(function(){
-    let $this = $(this);
+$(document).ready(function() {
 
-    $('.main_home_floating > a').removeClass('on');
-    $this.addClass('on');
-})
-
-// 스크롤 시 플로팅 메뉴 활성화 (Scrollspy)
-$(window).on('scroll', function() {
-    let scrollTop = $(window).scrollTop() + 200; 
-
-    $('.main_sec').each(function() {
-        let $this = $(this);
-        let sectionTop = $this.offset().top; 
-        let sectionBottom = sectionTop + $this.outerHeight(); 
-        let sectionId = $this.attr('id'); 
-
-        if (scrollTop >= sectionTop && scrollTop < sectionBottom) {
-            $('.main_home_floating > a').removeClass('on');
-            $('.main_home_floating > a[href="#' + sectionId + '"]').addClass('on');
-        }
+    $('.main_home_floating > a').click(function() {
+        $('.main_home_floating > a').removeClass('on');
+        $(this).addClass('on');
     });
+
+    $(window).on('scroll', function() {
+        let scrollTop = $(window).scrollTop() + 300; 
+
+        $('.main_sec').each(function() {
+            let $this = $(this);
+            let sectionTop = $this.offset().top; 
+            let sectionBottom = sectionTop + $this.outerHeight(); 
+            let sectionId = $this.attr('id'); 
+
+            if (scrollTop >= sectionTop && scrollTop < sectionBottom) {
+                $('.main_home_floating > a').removeClass('on');
+                $('.main_home_floating > a[href="#' + sectionId + '"]').addClass('on');
+            }
+        });
+    });
+
 });
 
     //main_sec2 hover
